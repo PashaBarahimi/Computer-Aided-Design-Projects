34986167946fc9ffe53cc3d86cd2147b0095d0a5
MisaghM <Misagh.MHGG@gmail.com>
2023-01-28 23:03:33 +0330

    CA2 initial commit

---
+++ CA2/trunk/sim/sim_top.tcl
@@ -0,0 +1,36 @@
+alias clc ".main clear"
+
+clc
+exec vlib work
+vmap work work
+
+set TB					"TB"
+set hdl_path			"../src/hdl"
+set inc_path			"../src/inc"
+
+set run_time			"-all"
+
+#============================ Add verilog files  ===============================
+
+vlog 	+acc -incr -source  +define+SIM 	$hdl_path/file.v
+
+vlog 	+acc -incr -source  +incdir+$inc_path +define+SIM ./tb/tb.sv
+onerror {break}
+
+#================================ simulation ====================================
+
+vsim	-voptargs=+acc -debugDB $TB
+
+#======================= adding signals to wave window ==========================
+
+add wave -hex -group 	 	{TB}				sim:/$TB/*
+add wave -hex -group 	 	{top}				sim:/$TB/main/*
+add wave -hex -group -r		{all}				sim:/$TB/*
+
+#=========================== Configure wave signals =============================
+
+configure wave -signalnamewidth 2
+
+#====================================== run =====================================
+
+run $run_time
