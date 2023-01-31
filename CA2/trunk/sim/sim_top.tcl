03c1035a46915cf01a28f7e78281810a05c81647
Pasha Barahimi <pashabarahimi@gmail.com>
2023-01-31 20:19:08 +0330

    Add CA2 testbench

--- CA2/trunk/sim/sim_top.tcl
+++ CA2/trunk/sim/sim_top.tcl
@@ -4,7 +4,7 @@ clc
 exec vlib work
 vmap work work
 
-set TB					"TB"
+set TB					"EncoderTB"
 set hdl_path			"../src/hdl"
 set inc_path			"../src/inc"
 
@@ -12,9 +12,39 @@ set run_time			"-all"
 
 #============================ Add verilog files  ===============================
 
-vlog 	+acc -incr -source  +define+SIM 	$hdl_path/file.v
+vlog 	+acc -incr -source  +define+SIM 	$hdl_path/adder.v
+vlog 	+acc -incr -source  +define+SIM 	$hdl_path/addrc_controller.v
+vlog 	+acc -incr -source  +define+SIM 	$hdl_path/addrc_datapath.v
+vlog 	+acc -incr -source  +define+SIM 	$hdl_path/addrc_datapath.v
+vlog 	+acc -incr -source  +define+SIM 	$hdl_path/addrc.v
+vlog 	+acc -incr -source  +define+SIM 	$hdl_path/colparity_calc.v
+vlog 	+acc -incr -source  +define+SIM 	$hdl_path/colparity_controller.v
+vlog 	+acc -incr -source  +define+SIM 	$hdl_path/colparity_datapath.v
+vlog 	+acc -incr -source  +define+SIM 	$hdl_path/colparity.v
+vlog 	+acc -incr -source  +define+SIM 	$hdl_path/counter_modn.v
+vlog 	+acc -incr -source  +define+SIM 	$hdl_path/encoder_controller.v
+vlog 	+acc -incr -source  +define+SIM 	$hdl_path/encoder_datapath.v
+vlog 	+acc -incr -source  +define+SIM 	$hdl_path/encoder.v
+vlog 	+acc -incr -source  +define+SIM 	$hdl_path/five_bit_selector.v
+vlog 	+acc -incr -source  +define+SIM 	$hdl_path/mapper.v
+vlog 	+acc -incr -source  +define+SIM 	$hdl_path/matrix_mult.v
+vlog 	+acc -incr -source  +define+SIM 	$hdl_path/memory.v
+vlog 	+acc -incr -source  +define+SIM 	$hdl_path/mux.v
+vlog 	+acc -incr -source  +define+SIM 	$hdl_path/parity_calc.v
+vlog 	+acc -incr -source  +define+SIM 	$hdl_path/permutation_controller.v
+vlog 	+acc -incr -source  +define+SIM 	$hdl_path/permutation_datapath.v
+vlog 	+acc -incr -source  +define+SIM 	$hdl_path/permutation.v
+vlog 	+acc -incr -source  +define+SIM 	$hdl_path/register.v
+vlog 	+acc -incr -source  +define+SIM 	$hdl_path/revaluate_controller.v
+vlog 	+acc -incr -source  +define+SIM 	$hdl_path/revaluate_datapath.v
+vlog 	+acc -incr -source  +define+SIM 	$hdl_path/revaluate.v
+vlog 	+acc -incr -source  +define+SIM 	$hdl_path/rotate_controller.v
+vlog 	+acc -incr -source  +define+SIM 	$hdl_path/rotate_datapath.v
+vlog 	+acc -incr -source  +define+SIM 	$hdl_path/rotate.v
+vlog 	+acc -incr -source  +define+SIM 	$hdl_path/shift_register.v
+vlog 	+acc -incr -source  +define+SIM 	$hdl_path/shift_size.v
 
-vlog 	+acc -incr -source  +incdir+$inc_path +define+SIM ./tb/tb.sv
+vlog 	+acc -incr -source  +incdir+$inc_path +define+SIM ./tb/encoder_tb.sv
 onerror {break}
 
 #================================ simulation ====================================
@@ -24,7 +54,7 @@ vsim	-voptargs=+acc -debugDB $TB
 #======================= adding signals to wave window ==========================
 
 add wave -hex -group 	 	{TB}				sim:/$TB/*
-add wave -hex -group 	 	{top}				sim:/$TB/main/*
+add wave -hex -group 	 	{top}				sim:/$TB/enc/*
 add wave -hex -group -r		{all}				sim:/$TB/*
 
 #=========================== Configure wave signals =============================
